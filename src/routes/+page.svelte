760af563d00d0db48cbd0390b08fa60e7a8dbf8e
selectdev <66577268+selectdev@users.noreply.github.com>
2023-01-14 08:25:25 -0600

    Added artists

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,10 @@
 <script lang="ts">
     export let data: any;
-    data = data;
+
+    let artists: String;
+
+    if (data.currentlyPlaying.error) artists = "";
+    else artists = data.currentlyPlaying.item.artists.map((artist: any) => { return artist.name }).join(", ");
 </script>
 
 <div id="currentlyPlaying">
@@ -12,5 +16,6 @@
         <img src={data.currentlyPlaying.item.album.images[0].url} />
         <h2 class="text-white">Title: {data.currentlyPlaying.item.name}</h2>
         <h3 class="text-white">Album: {data.currentlyPlaying.item.album.name}</h3>
+        <h3 class="text-white">Artist(s): {artists}</h3>
     {/if}
 </div>
