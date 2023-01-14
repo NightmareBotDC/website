bfeaefa20e9e4e86d56acb6c5226c2da69892f2b
selectdev <66577268+selectdev@users.noreply.github.com>
2023-01-14 08:20:41 -0600

    Fixed api issue

--- src/routes/+layout.server.js
+++ src/routes/+layout.server.js
@@ -1,9 +1,9 @@
-/*import cookie from "cookie";*/
+import cookie from "cookie";
 
 export const load = async ({ request, setHeaders }) => {
-    /*const cookies = cookie.parse(request.headers.get("cookie") || '');*/
+    const cookies = cookie.parse(request.headers.get("cookie") || '');
 
-    const currentlyPlaying = await fetch("https://api.nightmarebot.tk/spotify/playing").then((res) => res.json());
+    const currentlyPlaying = await fetch("https://api.nightmarebot.tk/api/spotify/playing").then((res) => res.json());
 
     return {
         currentlyPlaying: currentlyPlaying
