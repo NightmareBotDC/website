798c2bcc428e007afc92ab0e3fe06b34e12deabc
selectdev <66577268+selectdev@users.noreply.github.com>
2023-01-14 08:18:14 -0600

    Removed cookies

--- src/routes/+layout.server.js
+++ src/routes/+layout.server.js
@@ -1,7 +1,7 @@
-import cookie from "cookie";
+/*import cookie from "cookie";*/
 
 export const load = async ({ request, setHeaders }) => {
-    const cookies = cookie.parse(request.headers.get("cookie") || '');
+    /*const cookies = cookie.parse(request.headers.get("cookie") || '');*/
 
     const currentlyPlaying = await fetch("https://api.nightmarebot.tk/spotify/playing").then((res) => res.json());
 
