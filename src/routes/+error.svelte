453d23f20815d65ef39da9b8e59fe02d1bc912ba
selectdev <66577268+selectdev@users.noreply.github.com>
2023-01-14 16:19:44 -0600

    Updated error page to have meta

--- src/routes/+error.svelte
+++ src/routes/+error.svelte
@@ -1,6 +1,6 @@
 <script lang="ts">
 	import { page } from '$app/stores';
-        import Header from "./components/Header.svelte";
+        import Meta from "./components/Meta.svelte";
 
         export let data: any;
 
@@ -8,7 +8,7 @@
 	subText = 'But dont worry, you can find plenty of other things on our homepage.';
 </script>
 
-<Header Title={$page.status} Description={$page.error.message} User={data.user || null} />
+<Meta Title={$page.status} Description={$page.error.message} />
 
 <section class="flex items-center h-full p-16 dark:bg-gray-900 dark:text-gray-100">
 	<div class="container flex flex-col items-center justify-center px-5 mx-auto my-8">
