cc16a0afdccbdb752b6f712a926975c852072984
selectdev <66577268+selectdev@users.noreply.github.com>
2023-01-14 16:04:32 -0600

    Replaced text color to black

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -13,14 +13,14 @@
 <Header Title="Home" Description="Nightmare Bot is a personal assistant project that uses Artificial Intelligence and Machine Learning algorithms to solve problems." User={data.user || null} />
 
 <div id="currentlyPlaying">
-    <h2 class="text-white">Currently Playing:</h2>
+    <h2 class="text-black">Currently Playing:</h2>
 
     {#if data.currentlyPlaying.error}
-        <h4 class="text-white">{data.currentlyPlaying.error}</h4>
+        <h4 class="text-black">{data.currentlyPlaying.error}</h4>
     {:else}
         <img src={data.currentlyPlaying.item.album.images[0].url} />
-        <h2 class="text-white">Title: {data.currentlyPlaying.item.name}</h2>
-        <h3 class="text-white">Album: {data.currentlyPlaying.item.album.name}</h3>
-        <h3 class="text-white">Artist(s): {artists}</h3>
+        <h2 class="text-black">Title: {data.currentlyPlaying.item.name}</h2>
+        <h3 class="text-black">Album: {data.currentlyPlaying.item.album.name}</h3>
+        <h3 class="text-black">Artist(s): {artists}</h3>
     {/if}
 </div>
