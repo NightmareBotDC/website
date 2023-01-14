8dfdaae2f3d11655edec0f820400a0089c86ceee
selectdev <66577268+selectdev@users.noreply.github.com>
2023-01-14 08:12:08 -0600

    Removed artists

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -12,6 +12,5 @@
         <img src={data.currentlyPlaying.item.album.images[0].url} />
         <h2 class="text-white">Title: {data.currentlyPlaying.item.name}</h2>
         <h3 class="text-white">Album: {data.currentlyPlaying.item.album.name}</h3>
-        <h3 class="text-white">Artist(s): {data.currentlyPlaying.item.artists.map((artist: any) => { return artist.name }).join(", ")}</h3>
     {/if}
 </div>
