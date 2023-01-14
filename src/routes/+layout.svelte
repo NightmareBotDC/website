055e57181f9450fe15c04e37ce268aa856240983
selectdev <66577268+selectdev@users.noreply.github.com>
2023-01-14 16:22:00 -0600

    Update +layout.svelte

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,9 +1,16 @@
 <script>
 	import '../app.css';
+	import Header from './components/Header.svelte';
 
         export let data;
 </script>
 
-<section id="content">
+<header class="mt-4 ml-4">
+	<Header User={data.user || null} />
+</header>
+
+<div class="p-3"></div>
+
+<section id="content" class="m-4">
 	<slot />
 </section>
