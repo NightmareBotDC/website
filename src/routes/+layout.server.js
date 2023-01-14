fda03cb2e6db1a1dd0152244a3c5f4f36833266b
selectdev <66577268+selectdev@users.noreply.github.com>
2023-01-14 08:01:58 -0600

    Added some stuff

---
+++ src/routes/+layout.server.js
@@ -0,0 +1,11 @@
+import cookie from "cookie";
+
+export const load = async ({ request, setHeaders }) => {
+    const cookies = cookie.parse(request.headers.get("cookie") || '');
+
+    const currentlyPlaying = await fetch("https://api.nightmarebot.tk/spotify/playing").then((res) => res.json());
+
+    return {
+        currentlyPlaying: currentlyPlaying
+    };
+};
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,1 +1,21 @@
-<h2 class="text-white">This is the content section.</h2>
+<script lang="ts">
+    export let data: any;
+
+    let artists: String;
+
+    if (data.currentlyPlaying.error) artists = "";
+    else artists = data.currentlyPlaying.item.artists.map((artist: any) => { return artist.name }).join(", ");
+</script>
+
+<div id="currentlyPlaying">
+    <h2 class="text-white">Currently Playing:</h2>
+
+    {#if data.currentlyPlaying.error}
+        <h4 class="text-white">{data.currentlyPlaying.error}</h4>
+    {:else}
+        <img src={data.currentlyPlaying.item.album.images[0].url} />
+        <h2 class="text-white">Title: {data.currentlyPlaying.item.name}</h2>
+        <h3 class="text-white">Album: {data.currentlyPlaying.item.album.name}</h3>
+        <h3 class="text-white">Artist(s): {artists}</h3>
+    {/if}
+</div>
