577f5e3b8228a78f5f5b2edf3e8f5b40a501ed8f
selectdev <66577268+selectdev@users.noreply.github.com>
2023-01-14 08:10:21 -0600

    Possible fix for the internal error

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,10 +1,6 @@
 <script lang="ts">
     export let data: any;
-
-    let artists: String;
-
-    if (data.currentlyPlaying.error) artists = "";
-    else artists = data.currentlyPlaying.item.artists.map((artist: any) => { return artist.name }).join(", ");
+    data = data;
 </script>
 
 <div id="currentlyPlaying">
@@ -16,6 +12,6 @@
         <img src={data.currentlyPlaying.item.album.images[0].url} />
         <h2 class="text-white">Title: {data.currentlyPlaying.item.name}</h2>
         <h3 class="text-white">Album: {data.currentlyPlaying.item.album.name}</h3>
-        <h3 class="text-white">Artist(s): {artists}</h3>
+        <h3 class="text-white">Artist(s): {data.currentlyPlaying.item.artists.map((artist: any) => { return artist.name }).join(", ")}</h3>
     {/if}
 </div>
