735282351944adcc9aa2d7b8c0f19fb323666708
selectdev <66577268+selectdev@users.noreply.github.com>
2023-01-15 14:21:10 -0600

    Create SongCard.svelte

---
+++ src/routes/components/SongCard.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+   export let Title: String;
+   export let Album: String;
+   export let Artists: String;
+</script>
+
+<div class="SongCard">
+   <h3 class="text-white font-bold">{Title}</h3>
+   <h6 class="text-white">{Album}</h6>
+   <h5 class="text-white">{Artists}</h5>
+</div>
