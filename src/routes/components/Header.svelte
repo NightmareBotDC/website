545f03f63ba9de4c25d14bba721733c5fdb42d38
selectdev <66577268+selectdev@users.noreply.github.com>
2023-01-21 13:58:03 -0600

    Update Header.svelte

--- src/routes/components/Header.svelte
+++ src/routes/components/Header.svelte
@@ -38,7 +38,7 @@
 			<div class="flex h-16 items-center justify-between">
 				<div class="flex items-center">
 					<div class="flex-shrink-0">
-						<img class="h-8 w-8 rounded-half" src="/logo.png" alt="Nightmare Project" />
+						<img class="h-8 w-8 rounded-md" src="/logo.png" alt="Nightmare Project" />
 					</div>
 					<div class="hidden md:block">
 						<div class="ml-10 flex items-baseline space-x-4">
