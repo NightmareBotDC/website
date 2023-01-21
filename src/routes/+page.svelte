13905c9334c9f43e9e015db2c57a44f116ff758a
selectdev <66577268+selectdev@users.noreply.github.com>
2023-01-21 15:55:51 -0600

    Update +page.svelte

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -13,10 +13,6 @@
 <div id="currentlyPlaying">
 	{#if data.currentlyPlaying.error}
 	{:else}
-                <SongCard Title={data.currentlyPlaying.item.name} Album={data.currentlyPlaying.item.album.name} Artists={data.currentlyPlaying.item.artists
-				.map((artist) => {
-					return artist.name;
-				})
-				.join(', ')} AlbumArt={data.currentlyPlaying.item.album.images[0].url} ArtistImage={data.currentlyPlaying.item.artistData.images[0].url} />
+                <SongCard Title={data.currentlyPlaying.item.name} Album={data.currentlyPlaying.item.album.name} Artist={data.currentlyPlaying.item.artists[0].name} AlbumArt={data.currentlyPlaying.item.album.images[0].url} ArtistImage={data.currentlyPlaying.item.artistData.images[0].url} />
 	{/if}
 </div>
