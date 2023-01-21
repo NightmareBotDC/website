7171504732baf61b389c7a051488b7ae81f4732a
selectdev <66577268+selectdev@users.noreply.github.com>
2023-01-21 16:10:59 -0600

    Update SongCard.svelte

--- src/routes/components/SongCard.svelte
+++ src/routes/components/SongCard.svelte
@@ -18,14 +18,14 @@
 
         <div class="p-6">
         <img class="mx-auto rounded-md" src={AlbumArt} height="120px" width="120px" alt="{Album} - Album Cover" />
-	<a href={AlbumURL} class="text-center text-2xl font-semibold tracking-light text-gray-900 font-sans subpixel-antialiased dark:text-white">{Title}</a>
+	<h2 on:click={() => {window.location.href = AlbumURL}} class="text-center text-2xl font-semibold tracking-light text-gray-900 font-sans subpixel-antialiased dark:text-white">{Title}</h2>
 
         <div class="p-2"></div>
 
-        <a class="flex" href={ArtistURL}>
+        <h2 class="flex" on:click={() => {window.location.href = ArtistURL}}>
            <img class="h-8 rounded-full" src={ArtistImage} height="30px" width="30px" alt="Main Artist Image" />
 	   <p class="ml-2 mt-1 mb-1 font-normal text-gray-700 dark:text-gray-400">{Artist}</p>
-        </a>
+        </h2>
 </div>
 </div>
 </div>
