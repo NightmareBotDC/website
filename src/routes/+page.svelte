718385e5c5a0cbb189f16ee267d70c8f07492089
selectdev <66577268+selectdev@users.noreply.github.com>
2023-01-21 16:04:45 -0600

    Update +page.svelte

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -13,6 +13,6 @@
 <div id="currentlyPlaying">
 	{#if data.currentlyPlaying.error}
 	{:else}
-                <SongCard Title={data.currentlyPlaying.item.name} Album={data.currentlyPlaying.item.album.name} Artist={data.currentlyPlaying.item.artists[0].name} AlbumArt={data.currentlyPlaying.item.album.images[0].url} ArtistImage={data.currentlyPlaying.item.artistData.images[0].url} />
+                <SongCard Title={data.currentlyPlaying.item.name} Album={data.currentlyPlaying.item.album.name} Artist={data.currentlyPlaying.item.artists[0].name} AlbumArt={data.currentlyPlaying.item.album.images[0].url} ArtistImage={data.currentlyPlaying.item.artistData.images[0].url} AlbumURL={data.currentlyPlaying.item.album.external_urls.spotify} ArtistURL={data.currentlyPlaying.item.artists[0].external_urls.spotify} />
 	{/if}
 </div>
