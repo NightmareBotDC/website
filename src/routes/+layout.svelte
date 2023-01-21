4133e3ef7fd7ad73d4c307c73886016934c87bcf
selectdev <66577268+selectdev@users.noreply.github.com>
2023-01-21 14:14:20 -0600

    Update +layout.svelte

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,13 +1,19 @@
 <script>
 	import '../app.css';
 	import Header from './components/Header.svelte';
-
+        import navigating from "$app/navigation";
 	export let data;
 </script>
 
 <Header User={data.user || null} />
 
 <div class="p-3" />
+
+{#if navigating}
+   <h2 class="text-white">Loading...</h2>
+{:else}
+   <h2 class="text-white">Everything has loaded...</h2>
+{/if}
 
 <section id="content" class="m-4">
 	<slot />
