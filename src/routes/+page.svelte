e349bb6d3569e82f7922946043c62833448fe94a
selectdev <66577268+selectdev@users.noreply.github.com>
2023-01-23 08:10:27 -0600

    Update +page.svelte

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -23,3 +23,19 @@
 		/>
 	{/if}
 </div>
+
+<div id="recentlyPlayed">
+    {#if data.recentlyPlayed.error}{:else}
+             {#each data.recentlyPlayed.items as track}
+                  <SongCard
+			Title={track.song.item.name}
+			Album={track.song.item.album.name}
+			Artist={track.song.item.artists[0].name}
+			AlbumArt={track.song.item.album.images[0].url}
+			ArtistImage={track.artistData.images[0].url}
+			AlbumURL={track.song.item.album.external_urls.spotify}
+			ArtistURL={track.song.item.artists[0].external_urls.spotify}
+		/>
+             {/each}
+    {/if}
+</div>
