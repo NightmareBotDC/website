5606a2cfb15cbca8eaa19ee262c01def66fc9f19
selectdev <66577268+selectdev@users.noreply.github.com>
2023-01-23 22:22:30 -0600

    Removed loading thing

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,12 +1,6 @@
 <script lang="ts">
 	import '../app.css';
 	import Header from './components/Header.svelte';
-	import { BarLoader } from 'svelte-loading-spinners';
-
-	let loading = true;
-	setTimeout(() => {
-		loading = false;
-	}, 6000);
 
 	export let data: any;
 </script>
@@ -15,14 +9,6 @@
 
 <div class="p-3" />
 
-{#if loading}
-	<div class="m-4">
-                <slot style="display: none" />
-		<h2 class="text-white font-semibold">Loading...</h2>
-		<BarLoader size="120" color="#FF3E00" unit="px" duration="1s" />
-	</div>
-{:else}
-	<section id="content" class="m-4">
-		<slot />
-	</section>
-{/if}
+<section id="content" class="m-4">
+     <slot />
+</section>
