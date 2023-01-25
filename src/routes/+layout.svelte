8d0ab20895fe71a189df9204330403b2ee220bf5
selectdev <66577268+selectdev@users.noreply.github.com>
2023-01-25 14:51:26 -0600

    Update +layout.svelte

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,11 +1,13 @@
 <script lang="ts">
 	import '../app.css';
 	import Header from './components/Header.svelte';
+        import Status from './components/Status.svelte';
 
 	export let data: any;
 </script>
 
 <Header User={data.user || null} />
+<Status />
 
 <div class="p-3" />
 
