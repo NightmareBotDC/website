d4a016eb32ba85e71f8544c83f00332695f5d82c
selectdev <66577268+selectdev@users.noreply.github.com>
2023-02-01 17:51:48 -0600

    Added use license

---
+++ src/routes/legal/uselicense/+page.svelte
@@ -0,0 +1,35 @@
+<h2 class="text-white">
+USE LICENSE
+
+This Use License (“License”) is a legal agreement between you and Selectdev (“Licensor”) for the use of Nightmare Bot (“Product”).
+
+By downloading, installing, or using the Product, you agree to be bound by the terms and conditions of this License. If you do not agree to the terms and conditions of this License, do not download, install, or use the Product.
+
+1. License Grant. Licensor hereby grants you a revocable, non-exclusive, non-transferable, limited license to use the Product for any purpose, including commercial use, subject to the terms and conditions of this License. You may not sell, rent, lease, lend, redistribute, or sublicense the Product. You may not use the Product for any illegal or unauthorized purpose.
+
+2. Ownership. You acknowledge and agree that the Product and all intellectual property rights therein are the property of Licensor and its licensors. You shall not acquire any ownership rights in the Product.
+
+3. Termination. This License is effective until terminated. Your rights under this License will terminate automatically without notice from Licensor if you fail to comply with any term(s) of this License. Upon the termination of this License, you shall cease all use of the Product and delete all copies of the Product in your possession or control.
+
+4. Disclaimer of Warranty. The Product is provided on an “as is” and “as available” basis. Licensor makes no representations or warranties of any kind, express or implied, as to the operation of the Product or the information, content, materials, or products included on the Product. Licensor does not warrant that the Product will be error-free or uninterrupted.
+
+5. Limitation of Liability. In no event shall Licensor be liable for any damages whatsoever (including, without limitation, damages for loss of profits, business interruption, loss of information) arising out of the use of or inability to use the Product, even if Licensor has been advised of the possibility of such damages.
+
+6. Governing Law. This License shall be governed by and construed in accordance with the laws of the United States of America without giving effect to any principles of conflicts of law.
+
+7. Entire Agreement. This License constitutes the entire agreement between you and Licensor and supersedes all prior or contemporaneous communications and proposals, whether oral or written, between you and Licensor.
+
+8. Severability. If any provision of this License is found to be invalid or unenforceable, that provision shall be enforced to the maximum extent possible, and the remaining provisions shall remain in full force and effect.
+
+9. Waiver. The failure of Licensor to exercise or enforce any right or provision of this License shall not constitute a waiver of such right or provision.
+
+10. Amendments. This License may not be amended or modified except in writing signed by both parties.
+
+11. No Assignment. You may not assign or transfer your rights or obligations under this License without the prior written consent of Licensor.
+
+By downloading, installing, or using the Product, you represent that you have read this License and agree to be bound by its terms and conditions.
+
+12. Attribution. If you use the Product in any public or commercial way, you must attribute the Product to Licensor in the manner specified by Licensor.
+
+13. MIT License. This License is based on the MIT license, and you may use the Product under the terms
+</h2>
