2c0e1ce695a3a88b133f4c2acf805b7d2ce5f36e
selectdev <66577268+selectdev@users.noreply.github.com>
2023-02-01 18:03:33 -0600

    Added legal page

---
+++ src/routes/legal/+page.svelte
@@ -0,0 +1,10 @@
+<h2 class="text-white font-bold">Legal</h2>
+<h6 class="text-white">This page provides some links to our legal policy pages such as our Terms of Service.</h6>
+
+<div class="p-4"></div>
+
+<div id="legal">
+   <a class="text-red font-bold" href="/legal/terms">Terms of Service</a>
+   <a class="text-red font-bold" href="/legal/uselicense">Use License</a>
+   <a class="text-red font-bold" href="/legal/sourcelicense">Source License</a>
+</div>
