7f1c34aab8caa074d26c3a8434d282bf0fef7d93
selectdev <66577268+selectdev@users.noreply.github.com>
2023-03-13 11:07:39 -0500

    Update +server.ts

--- src/routes/onlyfoodz/auth/callback/+server.ts
+++ src/routes/onlyfoodz/auth/callback/+server.ts
@@ -1,12 +1,10 @@
-import { error } from '@sveltejs/kit';
-
-/** @type {import('./$types').RequestHandler} */
-export async function GET({ request, cookies, url }) {
-	const token = url.searchParams.get('token') || null;
+/** @type {import('./$types').PageLoad} */
+export async function load({ request, cookies, url }) {
+        const token = url.searchParams.get('token') || null;
 
 	if (!token) throw error(400, 'No authentication token was passed with this request.');
 
 	return {
-        dcToken: token
-    };
+           dcToken: token
+        };
 }
