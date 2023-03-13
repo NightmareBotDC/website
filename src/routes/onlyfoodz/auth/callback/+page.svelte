962dfc3342f983771082367c5c02280e2334e09d
selectdev <66577268+selectdev@users.noreply.github.com>
2023-03-13 11:24:39 -0500

    Update +page.svelte

--- src/routes/onlyfoodz/auth/callback/+page.svelte
+++ src/routes/onlyfoodz/auth/callback/+page.svelte
@@ -14,4 +14,6 @@
 <p class="text-white">To finish logging into Onlyfoodz, you need to do one final step!</p>
 <p class="text-white">Please run <span id="cmd" class="text-red-600 font-bold">/login token:{data.dcToken}</span> on Discord</p>
 
-<button on:click={copyCmd}>Copy command</button>
+<div class="p-2"></div>
+
+<button class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800" on:click={copyCmd}>Copy command</button>
